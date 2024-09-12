2dd60c4d39108bdac796d3f1bb7dc8f211ae02f8
daniel sieradski <dsieradski@cnyric.org>
2024-09-12 01:29:15 -0400

    home wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	/* region imports */
+	import CloseIcon from 'lucide-svelte/icons/x';
 	import { onMount } from 'svelte';
 
 	import type { CongregationMetaRecord, PagesRecord } from '$lib/types';
 
 	import Congregations from '$lib/components/congregations.svelte';
+	import * as Collapsible from '$lib/components/ui/collapsible';
+	import { t } from '$lib/i18n';
 	/* endregion imports */
 
 	/* region variables */
@@ -25,9 +28,17 @@
 	});
 </script>
 
-<section class="prose mx-auto mb-12">
-	{@html content?.content}
-</section>
+<Collapsible.Root>
+	<Collapsible.Trigger>
+		<CloseIcon size="18" />
+	</Collapsible.Trigger>
+	<Collapsible.Content>
+		<section class="prose mx-auto mb-12">
+			<h1 class="text-2xl">{$t('base.home.title')}</h1>
+			{@html content?.content}
+		</section>
+	</Collapsible.Content>
+</Collapsible.Root>
 
 {#if congregations}
 	<Congregations {congregations} />
