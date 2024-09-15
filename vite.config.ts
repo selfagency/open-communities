cd14e2e02d39b5e48b54621ca331399798404a89
daniel sieradski <dsieradski@cnyric.org>
2024-09-15 01:06:53 -0400

    derp

--- vite.config.ts
+++ vite.config.ts
@@ -28,8 +28,7 @@ export default defineConfig({
 			strategies: 'injectManifest',
 			injectManifest: {
 				globIgnores: ['server/*'],
-				globPatterns: ['client/**/*.{html,js,css,ico,png,svg,woff,woff2,ttf,xml,webmanifest}'],
-				swSrc: './src/service-worker.ts'
+				globPatterns: ['client/**/*.{html,js,css,ico,png,svg,woff,woff2,ttf,xml,webmanifest}']
 			},
 			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
 			manifest: {
