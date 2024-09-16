fffdf4a90146243fdea690bb17bbf0e54321a1c7
daniel sieradski <dsieradski@cnyric.org>
2024-09-16 13:34:17 -0400

    fix tabs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	/* region imports */
+	import { onMount } from 'svelte';
+
 	import { page } from '$app/stores';
 	import Login from '$lib/components/login/index.svelte';
 	import SignUp from '$lib/components/login/signup.svelte';
@@ -15,15 +17,17 @@
 	let tab: 'login' | 'signup' = 'login';
 	/* endregion variables */
 
-	/* region reactivity */
-	$: if ($page.url.searchParams.has('signUp') || $page.url.searchParams.has('verifyEmail')) {
-		tab = 'signup';
-	}
+	/* region lifecycle */
+	onMount(() => {
+		if ($page.url.searchParams.has('signUp') || $page.url.searchParams.has('verifyEmail')) {
+			tab = 'signup';
+		}
 
-	$: if ($page.url.searchParams.has('resetPassword')) {
-		tab = 'login';
-	}
-	/* endregion reactivity */
+		if ($page.url.searchParams.has('resetPassword')) {
+			tab = 'login';
+		}
+	});
+	/* endregion lifecycle */
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center" style="min-height: 50vh;">
