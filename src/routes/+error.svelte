d4bfa0bf955100cb201b86ee722836244515e1b1
Daniel Sieradski <daniel@self.agency>
2025-07-18 15:59:37 -0400

    denumerize

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -15,17 +15,17 @@
 	{#if page.status === 401 || page.status === 403}
 		{#await import('$lib/assets/401.svg?component') then { default: Error401 }}
 			<Error401 class="mx-auto w-full fill-slate-800" />
-			<h1 class="text-center text-4xl font-bold">{m['errors.401']()}</h1>
+			<h1 class="text-center text-4xl font-bold">{m['errors._401']()}</h1>
 		{/await}
 	{:else if page.status === 404}
 		{#await import('$lib/assets/404.svg?component') then { default: Error404 }}
 			<Error404 class="mx-auto w-full fill-slate-800" />
-			<h1 class="text-center text-4xl font-bold">{m['errors.404']()}</h1>
+			<h1 class="text-center text-4xl font-bold">{m['errors._404']()}</h1>
 		{/await}
 	{:else}
 		{#await import('$lib/assets/500.svg?component') then { default: Error500 }}
 			<Error500 class="mx-auto w-full max-w-72 fill-slate-800" />
-			<h1 class="text-center text-4xl font-bold">{m['errors.500']()}</h1>
+			<h1 class="text-center text-4xl font-bold">{m['errors._500']()}</h1>
 		{/await}
 	{/if}
 </section>
