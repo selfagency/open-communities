aba7965a8e60dd39dc6f7ec74bd63b4a4cf9effc
Daniel Sieradski <daniel@self.agency>
2025-08-03 21:59:14 -0400

    update

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,11 +5,9 @@ import { cleanResponse, handleError } from '$lib/server/api';
 export async function load({ locals }) {
 	const { api } = locals;
 	const client = api.authStore.record;
-	// const locale = client?.lang || 'en';
 
 	try {
 		const [content, congregations] = await Promise.all([
-			// api.collection('pages').getFirstListItem(`slug="home-${locale}"`, { fetch }),
 			api.collection('pages').getFirstListItem(`slug="home-en"`, { fetch }),
 
 			api.collection('congregationMeta').getFullList({
@@ -17,8 +15,6 @@ export async function load({ locals }) {
 				filter: client?.admin ? '' : 'visible=1'
 			})
 		]);
-
-		// log.info('homepage data', { congregations, content, countries });
 
 		return {
 			congregations: congregations.map((c) => cleanResponse(c)),
