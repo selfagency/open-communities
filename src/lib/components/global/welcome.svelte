0f4c600aaf1339a64b0073401d4fd1375bf05f41
Daniel Sieradski <2541728+selfagency@users.noreply.github.com>
2026-07-02 16:41:06 -0400

    fix: use inline style for background image instead of Tailwind arbitrary value

--- src/lib/components/global/welcome.svelte
+++ src/lib/components/global/welcome.svelte
@@ -1,28 +1,23 @@
 <script lang="ts">
-/* region imports */
-import { goto } from '$app/navigation';
-import Find from '$lib/assets/find.svg?component';
-import Inclusive from '$lib/assets/inclusive.svg?component';
-import { Button } from '$lib/components/ui/button';
-import { m } from '$lib/paraglide/messages';
-/* endregion imports */
+  /* region imports */
+  import { goto } from "$app/navigation";
+  import { Button } from "$lib/components/ui/button";
+  import { m } from "$lib/paraglide/messages";
+  import Crowd from "$lib/assets/crowd.png";
+  /* endregion imports */
 </script>
 
 <div
-  class="relative mt-4 mb-16 flex min-h-56 w-full flex-col items-center justify-center rounded-xl bg-muted p-12 sm:mt-12 sm:mb-24 space-y-4"
+  class="relative mt-4 mb-16 flex min-h-56 w-full flex-col items-center justify-center rounded-xl bg-muted p-12 sm:mt-12 sm:mb-24 space-y-4 bg-cover bg-bottom"
+  style="background-image: url({Crowd})"
 >
-  <Find
-    aria-hidden="true"
-    class="absolute -top-10 -left-5 hidden w-24 fill-foreground dark:fill-background sm:block  sm:w-32 lg:-left-10"
-  />
-  <Inclusive
-    aria-hidden="true"
-    class="absolute -right-5 -bottom-10 hidden w-24 fill-foreground dark:fill-background  sm:block sm:w-32 lg:-right-10"
-  />
-
-  <h1 class="pointer-events-none w-full text-center text-2xl sm:max-w-[80%] sm:text-3xl md:text-4xl lg:text-5xl">
+  <h1
+    class="pointer-events-none w-full text-center text-2xl sm:max-w-[80%] sm:text-3xl md:text-4xl lg:text-5xl"
+  >
     {m.home_blurb()}
   </h1>
 
-  <Button onclick={async () => await goto('/about')} variant="outline">{m.learnMore()}</Button>
+  <Button onclick={async () => await goto("/about")} variant="outline">
+    {m.learnMore()}
+  </Button>
 </div>
